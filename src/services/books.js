6c6ce8b381612d959dfe0e69a06a257616a25429
Caio Batista <caio_silvabatista@hotmail.com>
2022-02-06 05:49:11 -0300

    ♻️  Refactoring(providers and services): remove console log and change function name

--- src/services/books.js
+++ src/services/books.js
@@ -1,4 +1,4 @@
 import books from '~/providers/fetchBooks'
 
 export const requestListFromBooks = (page = 1) => books.get('/v1/books', { params: { page } })
-export const requestObjectFromBook = bookId => books.post(`/v1/books/${bookId}`)
+export const requestObjectFromBooks = bookId => books.get(`/v1/books/${bookId}`)
