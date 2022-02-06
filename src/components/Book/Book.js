eea57417189dd4064f28e83307d881ec9ab43319
Caio Batista <caio_silvabatista@hotmail.com>
2022-02-06 01:23:18 -0300

    ✨ Feat(component): create book component

---
+++ src/components/Book/Book.js
@@ -0,0 +1,77 @@
+import PropType from 'prop-types'
+import styled from 'styled-components'
+
+import Box from '~/components/Box'
+import Text from '~/components/Text'
+
+const Book = ({ title, imageUrl, authors, pageCount, publisher, published }) => {
+  return (
+    <StyledBox height={160} display='flex' padding='19px 16px'>
+      <img src={imageUrl} alt={`Capa do livro ${title}`} />
+      <Box display='flex' flexDirection='column' justifyContent='space-between' ml={16} overflow='auto'>
+        <Box>
+          <Text variant='small' fontWeight={500}>
+            {title}
+          </Text>
+
+          {authors.map(author => (
+            <Text
+              style={{ whiteSpace: 'nowrap', overflow: 'auto', textOverflow: 'ellipsis' }}
+              key={author.replace(/ /g, '')}
+              variant='tiny'
+              fontWeight={400}
+              color='#AB2680'
+            >
+              {author}
+            </Text>
+          ))}
+        </Box>
+
+        <Box>
+          <Text
+            style={{ whiteSpace: 'nowrap', overflow: 'auto', textOverflow: 'ellipsis' }}
+            variant='tiny'
+            fontWeight={400}
+            color='#999999'
+          >
+            {pageCount} páginas
+          </Text>
+          <Text
+            style={{ whiteSpace: 'nowrap', overflow: 'auto', textOverflow: 'ellipsis' }}
+            variant='tiny'
+            fontWeight={400}
+            color='#999999'
+          >
+            {publisher}
+          </Text>
+          <Text
+            style={{ whiteSpace: 'nowrap', overflow: 'auto', textOverflow: 'ellipsis' }}
+            variant='tiny'
+            fontWeight={400}
+            color='#999999'
+          >
+            Publicado em {published}
+          </Text>
+        </Box>
+      </Box>
+    </StyledBox>
+  )
+}
+
+const StyledBox = styled(Box)`
+  background: #ffffff;
+  box-shadow: 0px 6px 24px rgba(84, 16, 95, 0.13);
+  border-radius: 4px;
+`
+
+Book.propTypes = {
+  title: PropType.string,
+  authors: PropType.arrayOf(PropType.string),
+  pageCount: PropType.number,
+  publisher: PropType.string,
+  published: PropType.number,
+  imageUrl: PropType.string,
+  isLoading: PropType.bool
+}
+
+export default Book
